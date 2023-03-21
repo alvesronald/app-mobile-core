81ef21cf6137768a98550e0235c2c6fd13e94886
Ronald Silva Alves <ronald.alves@NMAC-3420.local>
2023-03-21 09:03:00 -0300

    feat: create HomeScreen and ShoppingScreen

---
+++ src/modules/Shopping/SecondScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export const SecondScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Shopping - Screen 2</Text>
+    </SafeAreaView>
+  );
+};
---
+++ src/modules/Shopping/index.tsx
@@ -0,0 +1,4 @@
+import {OneScreen} from './OneScreen';
+import {SecondScreen} from './SecondScreen';
+
+export {OneScreen, SecondScreen};
